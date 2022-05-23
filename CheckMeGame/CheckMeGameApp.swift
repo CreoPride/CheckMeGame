843bf6f1eaf3280df01b48e16e838c000b14d72e
Konstantin Fomenkov <kolij2005@gmail.com>
2022-05-23 18:10:52 +0300

    Initial Commit

---
+++ CheckMeGame/CheckMeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckMeGameApp.swift
+//  CheckMeGame
+//
+//  Created by Konstantin Fomenkov on 23.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CheckMeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
